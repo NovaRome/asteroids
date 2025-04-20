f404a9d4c3a1a93d9c496a947a85e58833ccffc8
NovaRome_Marc_Laboy <laboy496@gmail.com>
2025-04-19 22:25:44 -0400

    Update add cirle shape

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0  # Set the frame rate to 60 FPS
     #Game loop
     while True:
         for event in pygame.event.get():
@@ -12,6 +14,9 @@ def main():
         screen.fill((0, 0, 0))
         
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000.0  # Convert milliseconds to seconds
+    
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
